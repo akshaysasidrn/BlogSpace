46e056b95151e53437e521649fa036f4d63941b2
aksh93 <akshaysasidharan93@gmail.com>
2016-01-07 00:05:45 +0530

    Removed paginate function from profile

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,7 +2,7 @@ class UserController < ApplicationController
 
 def show
     @posts=Post.where("user_id=#{current_user.id}")
-    @posts = Post.paginate(:page => params[:page], :per_page => 4)
+    
  end
 
 end
